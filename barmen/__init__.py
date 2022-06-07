c55bb553edd826f405557cb7daec1b9ba44e3dc9
Anastasiia <nastiaproturenko5@gmail.com>
2022-06-07 17:13:34 +0200

    bootstrap

--- barmen/__init__.py
+++ barmen/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__, template_folder="pages")
+Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///barmen.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
